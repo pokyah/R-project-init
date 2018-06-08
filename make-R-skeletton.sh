ceeb06291b64689d93d23bd79280b1a7386475b9
pokyah <thomas.goossens@protonmail.com>
2018-06-08 09:54:53 +0200

    updating script with TOS and header

--- make-R-skeletton.sh
+++ make-R-skeletton.sh
@@ -15,7 +15,68 @@ GNU GPL V3
 EOM
 
 cat > $mydir/init.R << EOM
-## New Script 
+#'---
+#'author: "Thomas Goossens (CRA-W) - t.goossens@cra.wallonie.be"
+#'output: 
+#'  html_document:
+#'    theme: default
+#'    toc: false
+#'    toc_depth: 6
+#'    toc_float:
+#'      collapsed: false
+#'      smooth_scroll: true
+#'  revealjs::revealjs_presentation:
+#'    theme: white
+#'    highlight: pygments
+#'    center: true
+#'    incremental: true
+#'    transition: fade
+#'    self_contained: false
+#'    reveal_plugins: ["notes", "search"]
+#'  md_document:
+#'    variant: markdown_github
+#'    toc: false
+#'    toc_depth: 6
+#'  pdf_document: default  
+#'title: "<INSERT YOUR TITLE HERE"
+#'date: "`r format(Sys.time(), '%d %B, %Y')`"
+#'---
+
+#+ ---------------------------------
+#' ## Script preparation
+#' 
+#+ preparation, echo=FALSE, warning=FALSE, message=FALSE, error=FALSE, results='asis'
+
+# Avoid interference with old variables by cleaning the Global Environment
+rm(list=ls(all=TRUE))
+
+# Automagically set the wd and the root of all projects 
+if (!require("here")) install.packages("here")
+library(here)
+wd.chr <- here::here()
+
+# loading the library to manage all the other libraries
+if (!require("pacman")) install.packages("pacman")
+library(pacman)
+requiredPackages <- read.csv("./settings/requiredPackages.csv", quote = "", sep = ",", header=TRUE, stringsAsFactors=FALSE)
+p_load(char=requiredPackages$packageName, character.only=TRUE )
+p_loaded()
+
+# Dynamic Sourcing of all the required functions
+source(paste0("../../pokyah/R-utilities/R-utilities.R"))
+source_files_recursively.fun("./R")
+source_files_recursively.fun("../agrometeor_utilities_public/R/")
+
+#+ ---------------------------------
+#' ## Terms of service 
+#' To use the [AGROMET API](https://app.pameseb.be/fr/pages/api_call_test/) you need to provide your own user token.  
+#' The present script is available under the [GNU-GPL V3](https://www.gnu.org/licenses/gpl-3.0.en.html) license and comes with ABSOLUTELY NO WARRANTY.
+#' 
+#' Copyright : Thomas Goossens - t.goossens@cra.wallonie.be 2018.  
+#' 
+#' *(This document was generated using [R software](https://www.r-project.org/) with the [knitr library](https://deanattali.com/2015/03/24/knitrs-best-hidden-gem-spin/))*.  
+#+ TOS,echo=TRUE,warning=FALSE,message=FALSE,error=FALSE	
+
 EOM
 
 cat> $mydir/.gitignore << EOM
@@ -55,6 +116,7 @@ vignettes/*.pdf
 
 # Shiny token, see https://shiny.rstudio.com/articles/shinyapps.html
 rsconnect/
+
 EOM
 
 cat> $mydir/settings/requiredPackages.csv << EOM
